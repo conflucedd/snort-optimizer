98f87313d0c4bce016336e8cbf13aeb3ecab0b3b
confluced <confluced@gmail.com>
2026-05-01 10:57:43 +0000

    天才

--- analyser/cmd/command.sh
+++ analyser/cmd/command.sh
@@ -1,1 +1,1 @@
-./cmd --pcap1 ../../data/Tuesday.pcap --db1 ../../data/Tuesday.db --pcap2 ../../data/Monday.pcap --config ../../tmp/config/snort.lua --raw-snort-db snort.sqlite --workdir /tmp/test_ana
+./cmd --pcap1 ../../data/Tuesday.pcap --db1 ../../data/Tuesday.db --pcap2 ../../data/Monday.pcap --config ../../tmp/config/snort.lua --raw-snort-sqlite snort.sqlite --workdir /tmp/test_ana
